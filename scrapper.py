d8300f10baa7fd28701a9f7a62f4426b60f18d16
shaksham saxena <shaksham.saxena@nyalazone.com>
2022-10-01 23:12:39 +0530

    Scrapper Updated - Data Processing Completed; API Added to insert/update the data

--- scrapper.py
+++ scrapper.py
@@ -1,5 +1,8 @@
+import json
+import time
 from pprint import pprint
 
+import requests
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
@@ -15,7 +18,7 @@ def get_table_content():
     # Since Height of CoinMarketPlace is 10k+ and data loads on scroll - we are using scrollBy 1000 to complete
     # refreshed data
     javaScript = "window.scrollBy(0, 1000);"
-    for i in range(10):
+    for i in range(11):
         driver.execute_script(javaScript)
 
     table = driver.find_element(By.CLASS_NAME, 'bFzXgL')
@@ -25,15 +28,48 @@ def get_table_content():
 
 
 def parse_table_content(html_data):
-    thead = BeautifulSoup(html_data, features="html.parser")("thead")[0]("th")
+    # thead = ['name','price','1h','24h','7d','market_cap','volume24h','supply']
     tbody = BeautifulSoup(html_data, features="html.parser")("tbody")[0]("tr")
-    response = {
-        'head_data': [cell.getText() for cell in thead][2:-2],
-        'row_data': [[cell.text for cell in row("td")][2:-2] for row in tbody]
+    row_data = []
+    for row in tbody:
+        td_data = row('td')[2:-2]
+        try:
+            temp_data = {
+                'id': int(td_data[0].findAll('div', class_='hDTQEj')[0].getText()),
+                'name': td_data[0].findAll('p', class_="lhJnKD")[0].getText(),
+                'code': td_data[0].findAll('p', class_="emEbFH")[0].getText(),
+                'img': td_data[0]('img')[0]['src'],
+                'price': td_data[1].getText(),
+                '1h': td_data[2].getText(),
+                '24h': td_data[3].getText(),
+                '7d': td_data[4].getText(),
+                'market_cap': td_data[5].findAll('span', class_="iosgXe")[0].getText(),
+                'volume24h': td_data[6].getText(),
+                'supply': td_data[7].getText(),
+            }
+            row_data.append(temp_data)
+        except Exception as e:
+            pass
+
+    return row_data
+
+
+def update_coin_data(insert_flag):
+    _url = 'http://localhost:5000/coin/insert' if insert_flag else 'http://localhost:5000/coin/update'
+    payload = {
+        "data": parse_table_content(get_table_content())
     }
-    return response
+    x = requests.post(_url, json=payload)
+    return json.loads(x.text)
 
 
 if __name__ == '__main__':
-    data = parse_table_content(get_table_content())
-    print(data)
+    insert_flag = True  # this flag can be set in env so that we dont insert everytime when we restart scrapper
+    while True:
+        resp = update_coin_data(insert_flag)
+        if insert_flag:
+            if 'err_detail' in resp and 'duplicate key value violates unique constraint' in resp['err_detail']:
+                resp = update_coin_data(False)
+            insert_flag = False
+        print(resp)
+        time.sleep(5)  # Schedule Cron Job on Server to scrap intead on while loop
